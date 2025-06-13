82907c1db4fa3114d3d1895d4a04506e5439c664
toyokawa <sattsuttsu66-1@yahoo.co.jp>
2025-06-13 14:47:10 +0900

    20250613

--- page-nikai.php
+++ page-nikai.php
@@ -5,7 +5,7 @@
 			</div>
 		</section>
 		<section class="nikai-read">
-			<div class="contents-wrapper">
+			<div class="contents-wrapper fade-in">
 				<div class="nikai-read-wrapper">
 					<h1 class="nikai-read-title">信州でホルモン食べるならNIKAI</h1>
 					<p class="nikai-read-text">
@@ -18,16 +18,79 @@
 			<div class="nikai-miso-bg">
 				<img src="<?php echo get_template_directory_uri(); ?>/images/assets/nikai-miso-bg.jpg" alt="NIKAI-fv">
 			</div>
-			<div class="naka-miso-content">
-				<h3 class="nakai-miso-content-title">
+			<div class="nakai-miso-content fade-in">
+				<div class="nakai-miso-body color-white">
+					<h3 class="nakai-miso-body-title"><span>信州みそホルモン</span><span class="br-span">を楽しむ！</span></h3>
 					<p class="nakai-miso-text">その一口で広がる、旨味と香ばしさ。私たちNIKAIでは、厳選された新鮮なホルモンを使用し、こだわりの味噌ダレでじっくりと焼き上げます。信州の大自然に育まれた素材の味を、炭火で引き出した至福の瞬間をお楽しみください。仲間と囲む焼肉のひとときが、心に残る特別な思い出に。
 					</p>
-				</h3>
+				</div>
+			</div>
+		</section>
+
+		<section class="nikai-hormone-about">
+			<div class="contents-wrapper fade-in">
+				<h2 class="nikai-hormone-about-title">ホルモン<span>が</span>旨い理由</h2>
+				<ul class="nikai-hormone-about-lists color-white">
+					<li class="nikai-hormone-about-list">
+						<p class="nikai-about-list-number">01</p>
+						<p class="nikai-about-list-title">ホルモン</p>
+					</li>
+					<li class="nikai-hormone-about-list">
+						<p class="nikai-about-list-number">02</p>
+						<p class="nikai-about-list-title">焼き</p>
+					</li>
+					<li class="nikai-hormone-about-list">
+						<p class="nikai-about-list-number">03</p>
+						<p class="nikai-about-list-title">タレ</p>
+					</li>
+				</ul>
+				<ul class="nikai-hormone-about-items">
+					<li class="nikai-hormone-about-item">
+						<div class="nikai-about-item-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/assets/nikai-about-img01.jpg" alt="ホルモン">
+						</div>
+						<div class="nikai-about-item-body">
+							<h3 class="nikai-about-item-title nikai-about-number-one">
+								ホルモン
+							</h3>
+
+							<p class="nikai-about-item-text">
+								ホルモンはヘルシーで栄養満点な食材です。低脂肪でありながら旨味が凝縮されていて、食べ応えも抜群で豊かな味わいが広がります。食感の違いも楽しめるので、一度食べるとやみつきになること間違いなしです。
+							</p>
+						</div>
+					</li>
+					<li class="nikai-hormone-about-item">
+						<div class="nikai-about-item-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/assets/nikai-about-img02.jpg" alt="焼き">
+						</div>
+						<div class="nikai-about-item-body">
+							<h3 class="nikai-about-item-title nikai-about-number-two">
+								焼き
+							</h3>
+							<p class="nikai-about-item-text">
+								ホルモンは新鮮で旨味たっぷりな部位を提供し焼いたときのジューシーな味わいが特徴です。外はカリッと中は柔らかく、香ばしさが際立ちます。ビールや日本酒との相性も抜群で、食べるたびにお酒が進むこと間違いなし！
+							</p>
+						</div>
+					</li>
+					<li class="nikai-hormone-about-item">
+						<div class="nikai-about-item-img">
+							<img src="<?php echo get_template_directory_uri(); ?>/images/assets/nikai-about-img03.jpg" alt="タレ">
+						</div>
+						<div class="nikai-about-item-body">
+							<h3 class="nikai-about-item-title nikai-about-number-three">
+								タレ
+							</h3>
+							<p class="nikai-about-item-text">
+								深い旨味とまろやかな甘みが特徴の自家製タレ。信州産の味噌を使用して作り上げた濃厚な味わいがホルモンとの相性抜群。旨さを引き立て、食べるたびに満足感を与えてくれます。焼き立てホルモンと一緒に楽しむ最高の一品です。
+							</p>
+						</div>
+					</li>
+				</ul>
 			</div>
 		</section>
 
 		<section class="pig-hormone">
-			<div class="contents-wrapper">
+			<div class="contents-wrapper fade-in">
 				<h2 class="pig-hormone-title">まずは<br><span>豚ホルモン</span><span>を！</span></h2>
 				<p class="pig-hormone-comment">※価格はすべて税込です</p>
 				<div class="pig-hormone-content">
@@ -45,7 +108,7 @@
 		</section>
 
 		<section class="cow-hormone">
-			<div class="contents-wrapper">
+			<div class="contents-wrapper fade-in">
 				<h2 class="cow-hormone-title"><span>牛</span>ホルモン</h2>
 				<div class="cow-hormone-top">
 					<div class="hormone-top-img">
@@ -85,7 +148,7 @@
 
 
 		<section class="one-dish">
-			<div class="one-dish-wrapper contents-wrapper">
+			<div class="one-dish-wrapper contents-wrapper fade-in">
 				<div class="one-dish-content">
 					<div class="one-dish-body">
 						<h2 class="one-dish-title">
@@ -107,8 +170,8 @@
 
 		<section class=" nikai-drink">
 			<div class="nikai-drink-bg"></div>
-			<div class="contents-wrapper">
-				<div class="nikai-drink-wrapper">
+			<div class="contents-wrapper fade-in">
+				<div class="nikai-drink-wrapper ">
 					<h2 class="nikai-drink-title">ホルモンに<br class="sp">合わせて楽しむ極上<span>ドリンク</span></h2>
 
 					<div class="nikai-drink-content">
